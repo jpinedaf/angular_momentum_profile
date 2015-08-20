62e65d3c97cbf191209579490d7e1042be9b8a97
Jaime Pineda <jaime.e.pineda@gmail.com>
2015-08-20 18:46:49 +0200

    Updated plot
    -Updated results using latest fits

--- fit_NH3_lines.py
+++ fit_NH3_lines.py
@@ -13,17 +13,6 @@ from skimage.morphology import remove_small_objects,closing,disk,opening
 freq11= 23.6944955*u.GHz
 freq22= 23.7226336*u.GHz
 
-def fix_IRAS03282():
-    OneOneFile = 'fits_files/IRAS03282_Per5/IRAS03282.NH3.11.fits'
-    TwoTwoFile = 'fits_files/IRAS03282_Per5/IRAS03282.NH3.22.fits'
-    data, hd= fits.getdata( OneOneFile, header=True)
-    hd['CUNIT3'] = 'm/s'
-    fits.writeto(OneOneFile, data, hd, clobber=True)
-
-    data, hd= fits.getdata( TwoTwoFile, header=True)
-    hd['CUNIT3'] = 'm/s'
-    fits.writeto(TwoTwoFile, data, hd, clobber=True)
-
 def cubefit(region='IC348', do_plot=True, 
             snr_min=5.0, multicore=1):
     """
@@ -59,11 +48,11 @@ def cubefit(region='IC348', do_plot=True,
         vmax=10.0
         rms=3e-3
     elif region == 'IRAS03282':
-        OneOneFile = 'fits_files/IRAS03282_Per5/IRAS03282_NH3_11.fits'
-        TwoTwoFile = 'fits_files/IRAS03282_Per5/IRAS03282_NH3_22.fits'
+        OneOneFile = 'IRAS03282/IRAS03282-11_cvel_clean_rob05.fits'
+        TwoTwoFile = 'IRAS03282/IRAS03282-11_cvel_clean_rob05.fits'
         vmin=6.0
-        vmax=9.0
-        rms=3e-1
+        vmax=8.5
+        rms=2.5e-3
     elif region == 'L1451mm':
         OneOneFile = 'L1451mm/L1451MM-11_cvel_clean_rob05.fits'
         TwoTwoFile = 'L1451mm/L1451MM-11_cvel_clean_rob05.fits'
@@ -169,70 +158,7 @@ def cubefit(region='IC348', do_plot=True,
     fitcubefile.header.update('CRVAL3',0)
     fitcubefile.header.update('CRPIX3',1)
     fitcubefile.writeto("{0}_parameter_maps_snr{1}.fits".format(region,snr_min),clobber=True)
-
-def test_cube():
-    import numpy as np
-    from spectral_cube import SpectralCube
-    import pyspeckit
-    import astropy.units as u
-
-    freq11= 23.6944955*u.GHz
-    OneOneFile = 'fits_files/IRAS03282_Per5/IRAS03282_NH3_11.fits'
-    cube11sc = SpectralCube.read(OneOneFile)
-    vcube11sc = cube11sc.with_spectral_unit(u.km/u.s, rest_value=freq11, velocity_convention='radio')
-    print cube11sc
-    print vcube11sc
-
-    snr = vcube11sc.filled_data[:].value/rms
-    peaksnr = np.max(snr,axis=0)
-    #rms = np.nanmedian(errmap11)
-    errmap11 = np.full( snr.shape[1:], rms)
-
-
-    planemask = (peaksnr>snr_min) # *(errmap11 < 0.15)
-    planemask = remove_small_objects(planemask,min_size=40)
-    planemask = opening(planemask,disk(1))
-
-    from pyspeckit.spectrum.units import SpectroscopicAxis
-    xarr = SpectroscopicAxis( cube11sc.spectral_axis,
-                             refX=freq11,
-                             velocity_convention='radio')
-
-    cube11  = pyspeckit.Cube(cube=cube11sc)
-    vcube11 = pyspeckit.Cube(cube=vcube11sc, xarr=xarr)
-    cube11.unit="K"
-    vcube11.unit="K"
-
-    guesses = np.zeros((6,)+cube11.cube.shape[1:])
-    guesses[0,:,:] = 12                    # Kinetic temperature 
-    guesses[1,:,:] = 8                     # Excitation  Temp
-    guesses[2,:,:] = 14.5                  # log(column)
-    guesses[3,:,:] = 0.4  # Line width / 5 (the NH3 moment overestimates linewidth)               
-    guesses[4,:,:] = 8.0  # Line centroid              
-    guesses[5,:,:] = 0.5                   # F(ortho) - ortho NH3 fraction (fixed)
-    F=False
-    T=True
-    print('start fit')
-    cube11.fiteach(fittype='ammonia',  guesses=guesses,
-                  integral=False, verbose_level=3, 
-                  fixed=[T,F,F,F,F,T], signal_cut=2,
-                  limitedmax=[F,F,F,F,T,T],
-                  maxpars=[0,0,0,0,9.0,1],
-                  limitedmin=[T,T,T,T,T,T],
-                  minpars=[5,2.8,12.0,0,6.0,0],
-                  start_from_point=(243,243),
-                  use_neighbor_as_guess=True)
-
-    print('start fit')
-    vcube11.fiteach(fittype='ammonia',  guesses=guesses,
-                  integral=False, verbose_level=3, 
-                  fixed=[T,F,F,F,F,T], signal_cut=2,
-                  limitedmax=[F,F,F,F,T,T],
-                  maxpars=[0,0,0,0,9.0,1],
-                  limitedmin=[T,T,T,T,T,T],
-                  minpars=[5,2.8,12.0,0,6.0,0],
-                  start_from_point=(243,243),
-                  use_neighbor_as_guess=True)
+    convert_param_file(region=region, snr_min=snr_min)
 
 def convert_param_file(region='L1451mm', snr_min=5.0):
     """
@@ -315,5 +241,5 @@ def convert_param_file(region='L1451mm', snr_min=5.0):
     param=cube[11,:,:]
     file_out="{0}_eFortho_snr{1}_v1.fits".format(region,snr_min)
     fits.writeto(file_out, param, hd, clobber=True)
-        #GAS.PropertyMaps.update_cubefit(region=region)
+    #GAS.PropertyMaps.update_cubefit(region=region)
         
